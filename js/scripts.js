b9e7c1fe45569874b364aab7175eb40083d8d9ee
JonteJatobokul <jthiesall@gmail.com>
2023-08-31 13:41:23 +0200

    2.3
    incorporating js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 console.log("Welcome to my portfolio website!");
+
+var name = "Jonte";
+var age = 21;
+var myobjects = {
+    name: name,
+    age: age,
+}
+
+console.log(myobjects);
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
