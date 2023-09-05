7e061bd2090791e6c42d223e21996e7a4a5f30c0
JonteJatobokul <jthiesall@gmail.com>
2023-09-05 12:08:57 +0200

    linting 2.5
    corrected mistakes discovered trough linting

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 console.log("Welcome to my portfolio website!");
-
-var name = "Jonte";
-var age = 21;
+var name = "Jonte"; var age = 21;
 var myobjects = {
     name: name,
     age: age,
 }
-
 console.log(myobjects);
-
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
